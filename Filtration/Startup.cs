1c07c9a935286a582d6989c5cbf86160ae08e868
Kamil <ushurbakiev.k@gmail.com>
2018-10-12 21:14:55 +0600

    Add project files.

---
+++ Filtration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Filtration.Startup))]
+namespace Filtration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
